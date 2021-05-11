e23434ab2bf00923e339741804b25f58a214eebd
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-05-11 11:57:53 -0400

    finished Class Inheritance Project

--- manager.rb
+++ manager.rb
@@ -1,28 +1,43 @@
-require_relative "employee.rb"
+require_relative "employee"
 
 class Manager < Employee 
 
-    def initialize
+    attr_reader :employees
+
+    def initialize(name, salary, title, boss)
+        super(name, salary, title, boss=nil)
         @employees = []
-        super(@boss = nil)
+        # super(@boss = nil)
     end
 
     def add_employees(employee)
         @employees << employee
     end
 
-    def bonus(mulitplier)
+    def bonus(multiplier)
         total = 0
         @employees.each do |employee|
+            total += employee.salary
+
             if employee.is_a? Manager 
                 employee.employees.each do |employee_2|
-                    total += employees_2.salary
+                    total += employee_2.salary
                 end
-            else 
-                total += employee.salary 
-            end
+            end             
         end
-        total * mulitplier
+        total * multiplier
     end
 end
 
+ned = Manager.new("Ned", 1000000, "Founder", nil)
+darren = Manager.new("Darren", 78000, "TA Manager", ned)
+first = Employee.new("David", 10000, "TA", darren)
+second = Employee.new("Shawna", 12000, "TA", darren)
+
+ned.add_employees(darren)
+darren.add_employees(first)
+darren.add_employees(second)
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p first.bonus(3) # => 30_000
